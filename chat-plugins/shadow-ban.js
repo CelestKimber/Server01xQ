32e52f87501b4b2bc2cd569e0f1f39eb48ef25cf
CelestKimber <celestkimberly@gmail.com>
2016-12-18 11:36:10 -0700

    Update shadow-ban.js

--- chat-plugins/shadow-ban.js
+++ chat-plugins/shadow-ban.js
@@ -1,36 +1,40 @@
-const ROOM_NAME = "Moon";
-var room = Rooms.get(toId(ROOM_NAME));
+'use strict';
+
+const ROOM_NAME = "Shadow Ban Room";
+let room = Rooms.get(toId(ROOM_NAME));
 if (!room) {
 	Rooms.global.addChatRoom(ROOM_NAME);
 	room = Rooms.get(toId(ROOM_NAME));
 
 	room.isPrivate = true;
 	room.staffRoom = true;
+	room.staffAutojoin = true;
 	room.addedUsers = {};
 
 	if (room.chatRoomData) {
 		room.chatRoomData.isPrivate = true;
 		room.chatRoomData.staffRoom = true;
+		room.chatRoomData.staffAutojoin = true;
 		room.chatRoomData.addedUsers = room.addedUsers;
 
 		Rooms.global.writeChatRoomData();
 	}
 }
-if (Object.size(room.addedUsers) > 0) {
+if (Object.keys(room.addedUsers).length > 0) {
 	setImmediate(function () {
-		room.add("||Cargando lista de usuarios castigados:: " + Object.keys(room.addedUsers).sort().join(", "));
+		room.add("||Loaded user list: " + Object.keys(room.addedUsers).sort().join(", "));
 		room.update();
 	});
 }
 exports.room = room;
 
 function getAllAlts(user) {
-	var targets = {};
+	let targets = {};
 	if (typeof user === 'string') {
 		targets[toId(user)] = 1;
 	} else {
 		user.getAlts().concat(user.name).forEach(function (altName) {
-			var alt = Users.get(altName);
+			let alt = Users.get(altName);
 			if (!alt.named) return;
 
 			targets[toId(alt)] = 1;
@@ -43,14 +47,14 @@ function getAllAlts(user) {
 }
 
 function intersectAndExclude(a, b) {
-	var intersection = [];
-	var exclusionA = [];
-	var exclusionB = [];
+	let intersection = [];
+	let exclusionA = [];
+	let exclusionB = [];
 
-	var ai = 0;
-	var bi = 0;
+	let ai = 0;
+	let bi = 0;
 	while (ai < a.length && bi < b.length) {
-		var difference = a[ai].localeCompare(b[bi]);
+		let difference = a[ai].localeCompare(b[bi]);
 		if (difference < 0) {
 			exclusionA.push(a[ai]);
 			++ai;
@@ -69,18 +73,18 @@ function intersectAndExclude(a, b) {
 	return {intersection: intersection, exclusionA: exclusionA, exclusionB: exclusionB};
 }
 
-var checkBannedCache = {};
-var checkBanned = exports.checkBanned = function (user) {
-	var userId = toId(user);
+let checkBannedCache = {};
+exports.checkBanned = function (user) {
+	let userId = toId(user);
 	if (userId in checkBannedCache) return checkBannedCache[userId];
 	//console.log("Shadow ban cache miss:", userId);
 
-	var targets = Object.keys(getAllAlts(user)).sort();
-	var bannedUsers = Object.keys(room.addedUsers).sort();
+	let targets = Object.keys(getAllAlts(user)).sort();
+	let bannedUsers = Object.keys(room.addedUsers).sort();
 
-	var matches = intersectAndExclude(targets, bannedUsers);
-	var isBanned = matches.intersection.length !== 0;
-	for (var t = 0; t < targets.length; ++t) {
+	let matches = intersectAndExclude(targets, bannedUsers);
+	let isBanned = matches.intersection.length !== 0;
+	for (let t = 0; t < targets.length; ++t) {
 		if (isBanned) room.addedUsers[targets[t]] = 1;
 		checkBannedCache[targets[t]] = isBanned;
 	}
@@ -88,15 +92,15 @@ var checkBanned = exports.checkBanned = function (user) {
 
 	if (matches.exclusionA.length > 0) {
 		Rooms.global.writeChatRoomData();
-		room.add("||Las Alts de: " + matches.intersection[0] + " han sido automaticamente añadidas: " + matches.exclusionA.join(", "));
+		room.add("||Alts of " + matches.intersection[0] + " automatically added: " + matches.exclusionA.join(", "));
 	}
 
 	return true;
 };
 
-var addUser = exports.addUser = function (user) {
-	var targets = getAllAlts(user);
-	for (var u in targets) {
+let addUser = exports.addUser = function (user) {
+	let targets = getAllAlts(user);
+	for (let u in targets) {
 		if (room.addedUsers[u]) {
 			delete targets[u];
 		} else {
@@ -108,15 +112,15 @@ var addUser = exports.addUser = function (user) {
 
 	if (targets.length > 0) {
 		Rooms.global.writeChatRoomData();
-		room.add("||Preparando el castigo de: " + targets.join(", "));
+		room.add("||Added users: " + targets.join(", "));
 		room.update();
 	}
 
 	return targets;
 };
-var removeUser = exports.removeUser = function (user) {
-	var targets = getAllAlts(user);
-	for (var u in targets) {
+let removeUser = exports.removeUser = function (user) {
+	let targets = getAllAlts(user);
+	for (let u in targets) {
 		if (!room.addedUsers[u]) {
 			delete targets[u];
 		} else {
@@ -128,74 +132,66 @@ var removeUser = exports.removeUser = function (user) {
 
 	if (targets.length > 0) {
 		Rooms.global.writeChatRoomData();
-		room.add("||Quitandole el castigo a: " + targets.join(", "));
+		room.add("||Removed users: " + targets.join(", "));
 		room.update();
 	}
 
 	return targets;
 };
 
-var addMessage = exports.addMessage = function (user, tag, message) {
+exports.addMessage = function (user, tag, message) {
 	room.add('|c|' + user.getIdentity() + '|__(' + tag + ')__ ' + message);
-	room.update();
-};
-var addEmoticonMessage = exports.addEmoticonMessage = function (user, message) {
-	room.add(message);
 	room.update();
 };
 
 exports.commands = {
-	moonban: 'shadowban',
+	spam: 'shadowban',
+	sban: 'shadowban',
 	shadowban: function (target, room, user) {
+		if (!target) return this.sendReply("/shadowban OR /sban [username], [secondary command], [reason] - Sends all the user's messages to the shadow ban room.");
 
-		if (!target) return this.sendReply("/moonban [nick], [razon] - Castiga a un usuario enviandolo a la luna.");
-		if (!target) return this.sendReply("/moonban [nick], [razon] - Castiga a un usuario enviando sus mensajes a la luna.");
-
-
-		var params = this.splitTarget(target).split(',');
-		var action = params[0].trim().toLowerCase();
-		var reason = params.slice(1).join(',').trim();
+		let params = this.splitTarget(target).split(',');
+		let action = params[0].trim().toLowerCase();
+		let reason = params.slice(1).join(',').trim();
 		if (!(action in CommandParser.commands)) {
 			action = 'mute';
 			reason = params.join(',').trim();
 		}
 
-		if (!this.targetUser) return this.sendReply("El usuario '" + this.targetUsername + "' no fue encontrado.");
+		if (!this.targetUser) return this.sendReply("User '" + this.targetUsername + "' not found.");
 		if (!this.can('lock', this.targetUser)) return;
 
-		var targets = addUser(this.targetUser);
+		let targets = addUser(this.targetUser);
 		if (targets.length === 0) {
-
-			return this.sendReply('||' + this.targetUsername + " ya estaba en la luna.");
-			return this.sendReply('||' + this.targetUsername + " ya estaban sus mensajes en la luna.");
+			return this.sendReply('||' + this.targetUsername + " is already shadow banned or isn't named.");
 		}
-		this.privateModCommand("(" + user.name + " ha enviado a la luna a " + targets.join(", ") + (reason ? " (" + reason + ")" : "") + ")");
+		this.privateModCommand("(" + user.name + " has shadow banned: " + targets.join(", ") + (reason ? " (" + reason + ")" : "") + ")");
 
 		//return this.parse('/' + action + ' ' + toId(this.targetUser) + ',' + reason);
 	},
 
-	unmoonban: 'unshadowban',
+	unspam: 'unshadowban',
+	unsban: 'unshadowban',
 	unshadowban: function (target, room, user) {
-		if (!target) return this.sendReply("/unmoonban [username] - Deshace el castigo de la luna que se le asigno a un usuario.");
+		if (!target) return this.sendReply("/unshadowban OR /unsban [username] - Undoes /shadowban (except the secondary command).");
 		this.splitTarget(target);
 
 		if (!this.can('lock')) return;
 
-		var targets = removeUser(this.targetUser || this.targetUsername);
+		let targets = removeUser(this.targetUser || this.targetUsername);
 		if (targets.length === 0) {
-			return this.sendReply('||' + this.targetUsername + " no ha sido castigado.");
+			return this.sendReply('||' + this.targetUsername + " is not shadow banned.");
 		}
-		this.privateModCommand("(" + user.name + " ha librado del castigo a " + targets.join(", ") + ")");
+		this.privateModCommand("(" + user.name + " has shadow unbanned: " + targets.join(", ") + ")");
 	},
-	
-	moonbanlist: 'sbanlist',
+
 	sbanlist: function (target, room, user) {
 		if (!target && !this.can('lock')) return this.sendReply("The command '/sbanlist' was unrecognized.  To send a message starting with '/sbanlist', type '//sbanlist'.");
 		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
 		if (!this.can('lock')) return false;
-		
+
 		Users.get(toId(user.name)).send('|popup| Here is a list of sbanned users: \n' + JSON.stringify(Rooms.rooms.shadowbanroom.chatRoomData, null, 2));
-	}
+	},
 };
 
 Users.ShadowBan = exports;
